c6930452420034b0ca50b935c17ce583a00eda30
Bianca Lazzaretti Motta <104904010+m0ttab@users.noreply.github.com>
2022-07-26 15:25:50 -0300

    Delete views directory

--- views/cursos/edit.blade.php
+++
@@ -1,7 +0,0 @@
-<form action='/cursos/update' method='post'>
-    <input type='hidden' name='_token' value='{{ csrf_token() }}'/>
-    <input type='text' value="{{ $curso->nome }}" name='nome' placeholder="Entre com o nome"/>
-    <input type='text' value="{{ $curso->nome_reduzido }}" name='nome_reduzido' placeholder="Entre com o nome reduzido"/>
-    <input type="hidden" value="{{ $curso->id }}" name="id"/>
-    <button type='submit'>Alterar</button>
-</form>
--- views/turmas/edit.blade.php
+++
@@ -1,8 +0,0 @@
-<form action='/empregados/update' method='post'>
-    <input type='hidden' name='_token' value='{{ csrf_token() }}'/>
-    <input type='text' value="{{ $empregado->nome }}" name='nome' placeholder="Entre com o nome"/>
-    <input type='text' value="{{ $empregado->cargo }}" name='cargo' placeholder="Entre com o cargo"/>
-    <input type='text' value="{{ $empregado->datanasc }}" name='datanasc' placeholder="Entre com a data de nascimento"/>
-    <input type="hidden" value="{{ $empregado->id }}" name="id"/>
-    <button type='submit'>Alterar</button>
-</form>
