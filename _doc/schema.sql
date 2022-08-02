070318ec3c39e20ca0826c388da3c6a07e715c77
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-08-02 17:14:51 -0300

    Add files via upload

--- _doc/schema.sql
+++ _doc/schema.sql
@@ -1,10 +1,10 @@
-use laravel;
+use transportes;
  
 drop table if exists cursos;
  
 create table cursos (
     id int not null auto_increment,
-    nome varchar(80) not null,
+    nome varchar(200) not null,
     nome_reduzido varchar(100) not null,
     primary key (id)
 );
@@ -13,9 +13,9 @@ drop table if exists periodos;
  
 create table periodos (
     id int not null auto_increment,
-    ano varchar(80) not null,
-    dt_inicio varchar(100) not null,
-    dt_fim varchar(100) not null,
+    ano varchar(50) not null,
+    dt_inicio date not null,
+    dt_fim date not null,
 
     primary key (id)
 );
@@ -29,4 +29,22 @@ create table turmas (
     primary key (id)
 );
 
-ALTER TABLE turmas ADD CONSTRAINT fk_curTurmas FOREIGN KEY (curso_id) REFERENCES cursos (id)
+drop table if exists respostas;
+
+create table respostas (
+    
+    id int not null auto_increment,
+    periodo_id int not null,
+    turma_id int not null,
+    uf_id int not null,
+    nome_aluno varchar(80) not null,
+    cpf int not null,
+    cidade varchar(200) not null,
+    cidade_id int not null,
+    uf varchar(200) not null,
+    transporte varchar(200) not null,
+    poder_publico_responsavel varchar(200) not null,
+    diferenca_paga int,
+    primary key(id)
+    
+);
